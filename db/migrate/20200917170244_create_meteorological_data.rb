382aeb62b1789db31b6f2e93cda231a0418bb5ed
kylejb <kjburda@gmail.com>
2020-09-17 16:21:48 -0400

    Revised datatypes to match expected attributes

--- db/migrate/20200917170244_create_meteorological_data.rb
+++ db/migrate/20200917170244_create_meteorological_data.rb
@@ -2,17 +2,18 @@ class CreateMeteorologicalData < ActiveRecord::Migration[6.0]
   def change
     create_table :meteorological_data do |t|
       t.references :buoy, null: false, foreign_key: true
-      t.string :wind_dir
-      t.string :wind_speed
-      t.string :wind_gust
-      t.string :wave_height
-      t.string :dom_wave_period
-      t.string :avg_wave_period
-      t.string :wave_dir
-      t.string :sea_pressure
-      t.string :air_temp
-      t.string :sea_temp
+      t.integer :wind_dir
+      t.float :wind_speed
+      t.float :wind_gust
+      t.float :wave_height
+      t.integer :dom_wave_period
+      t.float :avg_wave_period
+      t.integer :wave_dir
+      t.float :sea_pressure
+      t.float :air_temp
+      t.float :sea_temp
       t.string :tide
+      t.datetime :recorded_data_at
       
       t.timestamps
     end

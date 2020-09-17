e8d8744906ca54328fe50ed115134aa8b5ff0124
kylejb <kjburda@gmail.com>
2020-09-17 13:53:38 -0400

    Minor formatting

--- db/migrate/20200917012721_create_favorite_beaches.rb
+++ db/migrate/20200917012721_create_favorite_beaches.rb
@@ -1,8 +1,8 @@
 class CreateFavoriteBeaches < ActiveRecord::Migration[6.0]
   def change
     create_table :favorite_beaches do |t|
-      t.references :beach, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
+      t.references :beach, null: false, foreign_key: true
       t.string :name
 
       t.timestamps

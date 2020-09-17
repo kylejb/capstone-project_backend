017d01b25d07029c0853929fc1c202a2ce5ba838
kylejb <kjburda@gmail.com>
2020-09-17 13:56:22 -0400

    Moved to attributes to MeteorologicalData table

--- db/migrate/20200917025806_create_buoys.rb
+++ db/migrate/20200917025806_create_buoys.rb
@@ -6,17 +6,6 @@ class CreateBuoys < ActiveRecord::Migration[6.0]
       t.string :station_owner
       t.float :longitude
       t.float :latitude
-      t.string :wind_dir
-      t.string :wind_speed
-      t.string :wind_gust
-      t.string :wave_height
-      t.string :dom_wave_period
-      t.string :avg_wave_period
-      t.string :wave_dir
-      t.string :sea_pressure
-      t.string :air_temp
-      t.string :sea_temp
-      t.string :tide
 
       t.timestamps
     end

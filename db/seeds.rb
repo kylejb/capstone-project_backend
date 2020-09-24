b6da94f155d8b90fedd18a742ad1dd66d2c030c5
kylejb <kjburda@gmail.com>
2020-09-24 12:25:38 -0400

    Added comment: Refactor this file due to NDBCScraper

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@ puts "Seeding: Start..."
 
 
 # Create a buoy and sample row of "latest" data that will eventually be harvested autonomously
+#! Delete this manual entry and test associations when moving to Phase 2 testing; time permitting, also implement test modules
 buoy = Buoy.create!(station_code: "44065", station_name: "New York Harbor Entrance", station_owner: "National Data Buoy Center", latitude: 40.369, longitude: -73.703)
 buoy_mdata = MeteorologicalDatum.create!(wind_dir: 230, wind_speed: 1.0, wind_gust: 2.0, wave_height: 0.9, dom_wave_period: 11, avg_wave_period: 6.8, wave_dir: 114, sea_pressure: 1016.3, sea_temp: 22.1, recorded_data_at: DateTime.new(2020, 9, 17, 17, 50), buoy: buoy)
 

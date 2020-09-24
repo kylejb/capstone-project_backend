c0bbf61988363f035f166943ada0281183153bce
kylejb <kjburda@gmail.com>
2020-09-24 15:26:21 -0400

    Jwt related methods to be inherited by all child controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::API
+    before_action :authorized
+
+    def encode_token(payload)
+        JwtAuth::encode_token(payload)
+    end
+     
+    def decoded_token(headers = request.headers['Authorization'])
+        JwtAuth::decoded_token(headers)
+    end
+     
+    def current_user
+        if decoded_token
+            user_id = decoded_token[0]['user_id']
+            @user = User.find(user_id)
+        end
+    end
+    
+    def logged_in?
+        !!current_user
+    end
+    
+    def authorized
+        render json: { message: 'Please log in' }, status: :unauthorized unless self.logged_in?
+    end
 end

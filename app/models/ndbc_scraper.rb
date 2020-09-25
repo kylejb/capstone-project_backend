b3139fa77e1d9143c273e9d13b46236cb37905f3
kylejb <kjburda@gmail.com>
2020-09-25 00:18:13 -0400

    MVP Revisions: Scraper successfully saturated database; needs to be refactored and enabled as a background task (e.g., cron)

--- app/models/ndbc_scraper.rb
+++ app/models/ndbc_scraper.rb
@@ -13,7 +13,7 @@ module NdbcScraper
         stations = xml.xpath("//station")
 
         stations.each do |station|
-           Buoy.create!(station_code: station[:id], station_name: station[:name], station_owner: station[:owner], latitude: station[:lat], longitude: station[:lon], type: station[:type], state: stationelev: station[:elev], met: station[:met], dart: station[:dart], currents: station[:currents], waterquality: station[:waterquality] )
+           Buoy.create!(station_code: station[:id], station_name: station[:name], station_owner: station[:owner], latitude: station[:lat], longitude: station[:lon], pgm: station[:pgm], type: station[:type], elev: station[:elev], met: station[:met], dart: station[:dart], currents: station[:currents], waterquality: station[:waterquality] )
         end
     end
 
@@ -24,6 +24,62 @@ module NdbcScraper
         doc = Nokogiri::HTML(html)
 
         station_pages = doc.css('pre a').map { |link| link['href'] }
-        puts station_pages
+    end
+
+    def self.realtime_meteorological_data(station_id)
+        realtime_url = "https://www.ndbc.noaa.gov/data/realtime2/#{station_id}.txt"
+        begin
+            content = URI.open(realtime_url).read
+            arr = []
+            content.each_line { |row| arr.push(row)}
+            self.fetch_data(station_id, arr)
+        rescue
+            puts "404"
+            nil
+        end
+    end
+
+    def self.parse_realtime_meteorological_data(data)
+        data.map { |row| row.split(" ") }#.transpose
+    end
+
+    def self.fetch_data(station_id, data)
+        buoy = Buoy.find_by(station_code: station_id)
+        data_arr_table = self.parse_realtime_meteorological_data(data)
+        data_arr_table.shift(2)
+        data_arr_table.each_with_index do | row, row_idx |
+            puts "#{row}  #{row_idx}"
+            # requires refactoring column names to correspond to incoming data
+            # row.each_with_index do | datum, data_idx |
+            time_s = row[0] + '-' + row[1] + '-' + row[2] + ' ' + row[3] + ':' + row[4]
+            record = buoy.meteorological_data.build(
+                recorded_data_at: DateTime.parse(time_s),
+                wind_dir: row[5].to_i,
+                wind_speed:row[6].to_f,
+                wind_gust: row[7].to_f,
+                wave_height: row[8].to_f,
+                dom_wave_period: row[9].to_i,
+                avg_wave_period: row[10].to_f,
+                wave_dir: row[11].to_i,
+                sea_pressure: row[12].to_f,
+                air_temp: row[13].to_f,
+                sea_temp: row[14].to_f,
+                dewpoint_temp: row[15],
+                station_visibility: row[16].to_f,
+                pressure_tendency: row[17],
+                tide: row[18],
+            )
+            # should include error handling output for controller/cron task
+            if record.valid?
+                record.save
+            end
+            # end
+        end
+    end
+
+    def self.run
+        Buoy.all.each do |buoy_obj|
+            self.realtime_meteorological_data(buoy_obj[:station_code])
+        end
     end
 end

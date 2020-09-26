1d510a62d59e08e2511a99a486d5c5184cbbed95
kylejb <kjburda@gmail.com>
2020-09-26 13:49:31 -0400

    Minor changes to limit nested data; will experiment with nesting route

--- app/controllers/api/v1/buoys_controller.rb
+++ app/controllers/api/v1/buoys_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::BuoysController < ApplicationController
 
+    # temp for testing
+    skip_before_action :authorized
+
     def index
         buoys = Buoy.all
 
@@ -7,8 +10,8 @@ class Api::V1::BuoysController < ApplicationController
     end
 
     def show
-        buoy = Buoy.first
+        buoy = Buoy.find(params[:id])
 
-        render json: buoy
+        render json: buoy.to_json(:include => :meteorological_data)
     end
 end

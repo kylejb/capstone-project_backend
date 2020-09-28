8192fd18e5c180bf7054e56050b0207f38e9ea9b
kylejb <kjburda@gmail.com>
2020-09-28 14:53:50 -0400

    Updated serializer to reflect model's updated relationship

--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -1,6 +1,7 @@
 class EntrySerializer < ApplicationSerializer
   attributes :id, :entry, :session_start_time, :wave_quality
   belongs_to :beach
+  belongs_to :buoy
 
   # def beach
   #   ActiveModel::SerializableResource.new(object.beach, each_serializer: BuoySerializer)

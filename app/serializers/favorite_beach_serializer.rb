08d74f4886edcf6d2228082024016b82a7744624
kylejb <kjburda@gmail.com>
2020-09-28 12:34:00 -0400

    Moved to '/storage' temporarily; focusing on FavBuoy this week

--- app/serializers/favorite_beach_serializer.rb
+++
@@ -1,7 +0,0 @@
-class FavoriteBeachSerializer < ApplicationSerializer
-  attributes :id, :name, :beach
-
-  def beach
-    ActiveModel::SerializableResource.new(object.beach, each_serializer: BeachSerializer)
-  end
-end

81929881e357e266cf386f8c0f6d427064ab7575
kylejb <kjburda@gmail.com>
2020-10-01 07:13:21 -0400

    Updated to comport with latest changes to models

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,25 +14,29 @@ puts "================================"
 puts "Successfully deleted previously seeded data from db"
 puts "Seeding: Start..."
 
+
 # Find a buoy and sample row of "latest" data that will eventually be harvested autonomously
 buoy = Buoy.find_by(station_code: "44065")
-buoy_mdata = MeteorologicalDatum.create!(wind_dir: 230, wind_speed: 1.0, wind_gust: 2.0, wave_height: 0.9, dom_wave_period: 11, avg_wave_period: 6.8, wave_dir: 114, sea_pressure: 1016.3, sea_temp: 22.1, recorded_data_at: DateTime.new(2020, 9, 17, 17, 50), buoy: buoy)
 
 
 # Create beach
 rockaway = Beach.create!(name: "Rockaway Beach", latitude: 40.5834115, longitude: -73.8241093)
 long_beach = Beach.create!(name: "Long Beach", latitude: 40.5895, longitude: -73.6665)
+
 # Create user
 kyle = User.create!(name: "Kyle", email: "kyle@email.com", password: "password")
 
 
 # Create association between a beach and user
-FavoriteBeach.create!(beach: rockaway, user: kyle)
+# FavoriteBeach.create!(beach: rockaway, user: kyle)
+
 # Create association between beach and buoy
 BuoyBeach.create!(buoy: buoy, beach: rockaway)
 BuoyBeach.create!(buoy: buoy, beach: long_beach)
-# Create association amongst beach, user, and entry
-Entry.create!(user: kyle, buoy: buoy, beach: rockaway, session_start_time: DateTime.now, session_duration: 1, entry: "Tough paddle due to cross-shore currents, but had a few memorable rides", wave_quality: "average")
+
+# Create association amongst user, buoy, and entry (beach is optional)
+Entry.create!(user: kyle, buoy: buoy, session_start_time: DateTime.now, session_duration: 1, entry: "Tough paddle due to cross-shore currents, but had a few memorable rides", wave_quality: "5")
+
 
 puts "================================"
 puts "... end"

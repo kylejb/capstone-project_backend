a4e9b1381aeb9c50cdab1e0aeb8e191e123f1c35
kylejb <kjburda@gmail.com>
2020-10-01 07:07:30 -0400

    Minor revision to ensure weather results are limited to the most recent

--- app/controllers/api/v1/buoy_meteorological_data_controller.rb
+++ app/controllers/api/v1/buoy_meteorological_data_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::BuoyMeteorologicalDataController < ApplicationController
 
     # currently limited to latest five reports; should the generation of reports be user triggered?
     def index
-        meteorological_data = Buoy.find(params[:buoy_id]).meteorological_data[1..5]
+        meteorological_data = Buoy.find(params[:buoy_id]).meteorological_data.order('recorded_data_at DESC')[2..6]
 
         render json: meteorological_data
     end
